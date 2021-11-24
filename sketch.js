17cd18a0507ffab28cfbb3f000dc804d48ee486d
vidhyani <vidhyanigandhi@gmail.com>
2021-11-24 12:57:33 +0530

    added coins

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 var mario,mario_running,mario_collided;
 var bg,bgImage,ground;
 var brickgroup,brickImg;
+var coinGroup,coinSound, coinImg,coinScore=0;
 
 function preload(){
 mario_running=loadAnimation("images/mar1.png","images/mar2.png","images/mar3.png","images/mar4.png","images/mar5.png","images/mar6.png","images/mar7.png");
 bgImage=loadImage("images/bgnew.jpg");
-brickImg=loadImage("images/brick.png")
+brickImg=loadImage("images/brick.png");
+coinImg=loadAnimation("images/con1.png","images/con2.png","images/con3.png","images/con4.png","images/con5.png","images/con6.png");
+coinSound=loadSound("sounds/coinSound.mp3")
 }
 
 function setup() {
@@ -22,7 +25,7 @@ ground.visible=false;
 mario.debug=false;
 
 brickgroup = new Group()
-
+coinGroup = new Group()
 
 }
 
@@ -55,8 +58,22 @@ if(mario.x<200){
 if(mario.y<50){
     mario.y=50;
 }
+generateCoins()
+for(var i=0;i<coinGroup.length;i++){
+    var coinTemp= coinGroup.get(i);
+    if(coinTemp.isTouching(mario)){
+        coinSound.play();
+        coinTemp.destroy();
+        coinTemp=null;
+        coinScore++;
+    }
+}
 
-drawSprites()    
+drawSprites()  
+fill("blue")
+textSize(20)  
+text("Coin Score: "+coinScore,500,50)
+
 }
 
 function generateBricks(){
@@ -68,5 +85,16 @@ function generateBricks(){
         brick.scale= 0.5;
         brickgroup.add(brick);
         brick.lifetime=300;
+    }
+}
+function generateCoins(){
+    if(frameCount%50===0){
+        var coin= createSprite(1200,120,40,10)
+        coin.addAnimation("coin",coinImg)
+        coin.velocityX=-3;
+        coin.scale=0.1;
+        coin.y=random(80,350)
+        coin.lifetime=1000;
+        coinGroup.add(coin)
     }
 }
